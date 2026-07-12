e99dbc962cb5fff8fb15f7b751bea910ee8a1027
Pratik Kumar Sinha <pratiksinha198@gmail.com>
2026-07-12 10:51:38 +0530

    add new features

--- Explorely/backend/models/State.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const stateSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  code: { type: String },
-});
-
-module.exports = mongoose.model('State', stateSchema);
